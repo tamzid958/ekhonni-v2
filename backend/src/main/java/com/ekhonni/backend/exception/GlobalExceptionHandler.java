f8546a827c11060491ff997eec71c02d7ab9fb80
jahidhasan1893 <jahidhasan1893@gmail.com>
2024-12-09 13:49:55 +0600

    update deleted item problem solved

--- backend/src/main/java/com/ekhonni/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ekhonni/backend/exception/GlobalExceptionHandler.java
@@ -1,35 +1,14 @@
 package com.ekhonni.backend.exception;
 
-import com.ekhonni.backend.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ApiResponse<?> handleEntityNotFoundException(EntityNotFoundException ex) {
-        return new ApiResponse<>(false, "Error", ex.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<?>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
-        var response = new ApiResponse<>(false, "Error", errors, HttpStatus.BAD_REQUEST);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-    }
-
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
@@ -37,23 +16,12 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.valueOf(404));
     }
 
-    @ExceptionHandler(InvalidTransactionException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidTransactionException(InvalidTransactionException ex) {
+    @ExceptionHandler(UserAlreadyDeletedException.class)
+    public ResponseEntity<ErrorResponse> handleUserAlreadyDeletedException(UserAlreadyDeletedException ex) {
         ErrorResponse response = new ErrorResponse(ex.getMessage(), LocalDateTime.now().toString());
         return new ResponseEntity<>(response, HttpStatus.valueOf(404));
     }
 
-    @ExceptionHandler(InitiatePaymentException.class)
-    public ResponseEntity<ErrorResponse> handleSSLCommerzPaymentException(InitiatePaymentException ex) {
-        ErrorResponse response = new ErrorResponse(ex.getMessage(), LocalDateTime.now().toString());
-        return new ResponseEntity<>(response, HttpStatus.valueOf(404));
-    }
-
-    @ExceptionHandler(BidLogNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleBidLogNotFoundException(BidLogNotFoundException ex) {
-        ErrorResponse response = new ErrorResponse(ex.getMessage(), LocalDateTime.now().toString());
-        return new ResponseEntity<>(response, HttpStatus.valueOf(404));
-    }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
@@ -63,12 +31,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccountNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleAccountNotFoundException(AccountNotFoundException ex) {
-        ErrorResponse response = new ErrorResponse(ex.getMessage(), LocalDateTime.now().toString());
-        return new ResponseEntity<>(response, HttpStatus.valueOf(404));
-    }
-
-    @ExceptionHandler(UnsupportedEncodingException.class)
-    public ResponseEntity<ErrorResponse> handleUnsupportedEncodingException(UnsupportedEncodingException ex) {
         ErrorResponse response = new ErrorResponse(ex.getMessage(), LocalDateTime.now().toString());
         return new ResponseEntity<>(response, HttpStatus.valueOf(404));
     }
--- backend/src/main/java/com/ekhonni/backend/service/UserService.java
+++ backend/src/main/java/com/ekhonni/backend/service/UserService.java
@@ -1,14 +1,13 @@
 package com.ekhonni.backend.service;
 
 import com.ekhonni.backend.dto.UserDTO;
+import com.ekhonni.backend.exception.UserAlreadyDeletedException;
 import com.ekhonni.backend.exception.UserNotFoundException;
 import com.ekhonni.backend.model.Account;
 import com.ekhonni.backend.model.User;
 import com.ekhonni.backend.projection.UserProjection;
 import com.ekhonni.backend.repository.AccountRepository;
-import com.ekhonni.backend.repository.BaseRepository;
 import com.ekhonni.backend.repository.UserRepository;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
@@ -19,64 +18,67 @@ import java.util.UUID;
 
 
 @Service
+@RequiredArgsConstructor
 @Setter
-public class UserService extends BaseService<User, UUID> {
+public class UserService {
     private final UserRepository userRepository;
     private final AccountRepository accountRepository;
 
-    public UserService(UserRepository userRepository, AccountRepository accountRepository) {
-        super(userRepository);
-        this.userRepository = userRepository;
-        this.accountRepository = accountRepository;
+    public List<UserProjection> getAll() {
+        return userRepository.findAllProjection();
     }
 
     public UserProjection getById(UUID id) {
-        return userRepository.findById(id, UserProjection.class)
-                .orElseThrow(UserNotFoundException::new);
+        if (userRepository.findById(id).isEmpty()) throw new UserNotFoundException();
+        if (userRepository.isDeleted(id)) throw new UserAlreadyDeletedException();
+        return userRepository.findProjectionById(id);
     }
 
     public UserDTO create(UserDTO userDTO) {
-        Account account = new Account(0.0, "Active");
         User user = new User(
                 userDTO.name(),
                 userDTO.email(),
                 userDTO.password(),
                 "USER",
                 userDTO.phone(),
-                userDTO.address(),
-                account
+                userDTO.address()
         );
         userRepository.save(user);
+
+        Account account = new Account(user, 0.0, "active");
         accountRepository.save(account);
+
         return userDTO;
     }
 
     public void createAll(List<UserDTO> userDTOs) {
         for (UserDTO userDTO : userDTOs) {
-            Account account = new Account(0.0, "Active");
             User user = new User(
                     userDTO.name(),
                     userDTO.email(),
                     userDTO.password(),
                     "USER",
                     userDTO.phone(),
-                    userDTO.address(),
-                    account
+                    userDTO.address()
             );
             userRepository.save(user);
+
+            Account account = new Account(user, 0.0, "Active");
             accountRepository.save(account);
         }
 
     }
 
     public void delete(UUID id) {
-        User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
-        userRepository.softDelete(id);
+        if (userRepository.isDeleted(id)) throw new UserAlreadyDeletedException();
+        if (userRepository.findById(id).isEmpty()) throw new UserNotFoundException();
+        userRepository.softDeleteById(id);
     }
 
 
     @Transactional
-    public UserDTO update(UUID id, UserDTO userDTO) {
+    public UserDTO update(UUID id, UserDTO userDTO) throws UserAlreadyDeletedException {
+        if (userRepository.isDeleted(id)) throw new UserAlreadyDeletedException();
         User user = userRepository.findById(id).orElseThrow(UserNotFoundException::new);
         if (userDTO.name() != null && !userDTO.name().isBlank()) {
             user.setName(userDTO.name());
@@ -96,5 +98,6 @@ public class UserService extends BaseService<User, UUID> {
 
         return userDTO;
     }
+
 
 }
