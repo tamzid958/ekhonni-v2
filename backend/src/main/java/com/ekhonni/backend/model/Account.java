acadec37091f1944541a56de54131b7ab2dba5c4
jahidhasan1893 <jahidhasan1893@gmail.com>
2024-12-10 16:50:40 +0600

    refactored code and security configuration used

--- backend/src/main/java/com/ekhonni/backend/model/Account.java
+++ backend/src/main/java/com/ekhonni/backend/model/Account.java
@@ -7,9 +7,11 @@ package com.ekhonni.backend.model;
 import com.ekhonni.backend.baseentity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -17,9 +19,6 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Account extends BaseEntity<Long> {
-    @NonNull
-    @OneToOne
-    private User user;
 
     @Column(nullable = false)
     private double balance = 0.0;
