09613499fd3e88b8632e9fe45d3a523f10a087c9
jahidhasan1893 <jahidhasan1893@gmail.com>
2024-12-12 17:47:45 +0600

    auth response refactored

--- backend/src/main/java/com/ekhonni/backend/controller/AuthController.java
+++ backend/src/main/java/com/ekhonni/backend/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.ekhonni.backend.dto.AuthDTO;
 import com.ekhonni.backend.dto.UserDTO;
 import com.ekhonni.backend.service.AuthService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -15,13 +17,39 @@ import org.springframework.web.bind.annotation.*;
 public record AuthController(AuthService authService) {
 
     @PostMapping("/sign-in")
-    public String signIn(@RequestBody AuthDTO authDTO) {
-        return authService.signIn(authDTO);
+    public ResponseEntity<?> signIn(@RequestBody AuthDTO authDTO) {
+
+        String token = authService.signIn(authDTO);
+
+        ResponseCookie cookie = ResponseCookie.from("JSESSIONID", token)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(7 * 24 * 60 * 60)
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header("Set-Cookie", cookie.toString())
+                .body("Login successful");
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/sign-up")
-    public String create(@RequestBody UserDTO userDTO) {
-        return authService.create(userDTO);
+    public ResponseEntity<?> create(@RequestBody UserDTO userDTO) {
+
+        String token = authService.create(userDTO);
+
+        ResponseCookie cookie = ResponseCookie.from("JSESSIONID", token)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(7 * 24 * 60 * 60)
+                .sameSite("Strict")
+                .build();
+
+        return ResponseEntity.ok()
+                .header("Set-Cookie", cookie.toString())
+                .body("Registration successful");
     }
 }
--- backend/src/main/java/com/ekhonni/backend/controller/UserController.java
+++ backend/src/main/java/com/ekhonni/backend/controller/UserController.java
@@ -3,7 +3,6 @@ package com.ekhonni.backend.controller;
 import com.ekhonni.backend.dto.UserDTO;
 import com.ekhonni.backend.projection.UserProjection;
 import com.ekhonni.backend.service.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,26 +18,22 @@ import java.util.UUID;
 public record UserController(UserService userService) {
 
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
     @GetMapping
     public List<UserProjection> getAll() {
         return userService.getAll();
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
     @GetMapping("/{id}")
     public UserProjection getById(@PathVariable UUID id) {
         return userService.getById(id);
     }
 
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
     @PatchMapping("/{id}/update")
     public UserDTO update(@PathVariable UUID id, @RequestBody UserDTO userDTO) {
         return userService.update(id, userDTO);
     }
 
-    @ResponseStatus(HttpStatus.ACCEPTED)
     @DeleteMapping("/{id}/delete")
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         userService.delete(id);
