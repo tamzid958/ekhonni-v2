f17071e5e235eec7586342db56a105206bddcb0b
jahidhasan1893 <jahidhasan1893@gmail.com>
2024-12-12 15:06:55 +0600

    jwt generation done

---
+++ backend/src/main/java/com/ekhonni/backend/util/JWTUtil.java
@@ -0,0 +1,52 @@
+package com.ekhonni.backend.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+/**
+ * Author: Md Jahid Hasan
+ * Date: 12/12/24
+ */
+
+@Service
+public class JWTUtil {
+
+    @Value("${spring.security.jwt.secret}")
+    private String secret;
+    @Value(("${spring.security.jwt.expiration}"))
+    private long expiration;
+
+
+    public String generate(Authentication authenticated) {
+        return Jwts
+                .builder()
+                .setSubject(authenticated.getName())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generate(String email) {
+        return Jwts
+                .builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public SecretKey generateKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
