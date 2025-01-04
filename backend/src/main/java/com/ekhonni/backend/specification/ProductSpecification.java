f7475970baac54f1b43d37f237343fca443238ed
shariar <shariarsakil101@gmail.com>
2025-01-05 01:04:02 +0600

    solve merge

---
+++ backend/src/main/java/com/ekhonni/backend/specification/ProductSpecification.java
@@ -0,0 +1,63 @@
+/**
+ * Author: Rifat Shariar Sakil
+ * Time: 11:24 AM
+ * Date: 12/22/2024
+ * Project Name: backend
+ */
+
+package com.ekhonni.backend.specification;
+
+
+import com.ekhonni.backend.enums.ProductCondition;
+import com.ekhonni.backend.enums.ProductSort;
+import com.ekhonni.backend.model.Product;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+
+public class ProductSpecification {
+    public static Specification<Product> belongsToCategories(List<Long> categoryIds) {
+        return (product, cq, cb) -> product.join("category").get("id").in(categoryIds);
+    }
+
+    public static Specification<Product> hasMinimumPrice(Double minPrice) {
+        return (product, cq, cb) -> cb.greaterThanOrEqualTo(product.get("price"), minPrice);
+    }
+
+    public static Specification<Product> hasMaximumPrice(Double maxPrice) {
+        return (product, cq, cb) -> cb.lessThanOrEqualTo(product.get("price"), maxPrice);
+    }
+
+    public static Specification<Product> hasCondition(ProductCondition condition) {
+        return (product, cq, cb) -> cb.equal(product.get("condition"), condition);
+    }
+
+
+    public static Specification<Product> applySorting(ProductSort sortBy) {
+        return (product, cq, cb) -> {
+            switch (sortBy) {
+                case priceLowToHigh:
+                    cq.orderBy(cb.asc(product.get("price")));
+                    break;
+                case priceHighToLow:
+                    cq.orderBy(cb.desc(product.get("price")));
+                    break;
+                case newlyListed:
+                    cq.orderBy(cb.desc(product.get("createdAt")));
+            }
+            return cb.conjunction();
+        };
+    }
+
+    public static Specification<Product> hasTerm(String searchTerm) {
+        return (product, cq, cb) -> {
+            String searchTermLower = "%" + searchTerm.toLowerCase() + "%";
+            return cb.or(
+                    cb.like(cb.lower(product.get("name")), searchTermLower),
+                    cb.like(cb.lower(product.get("category").get("name")), searchTermLower),
+                    cb.like(cb.lower(product.get("description")), searchTermLower)
+            );
+        };
+    }
+}
---
+++ backend/src/main/java/com/ekhonni/backend/specificationbuilder/ProductSpecificationBuilder.java
@@ -0,0 +1,42 @@
+/**
+ * Author: Rifat Shariar Sakil
+ * Time: 3:18 PM
+ * Date: 1/4/2025
+ * Project Name: backend
+ */
+
+package com.ekhonni.backend.specificationbuilder;
+
+import com.ekhonni.backend.enums.ProductCondition;
+import com.ekhonni.backend.filter.ProductFilter;
+import com.ekhonni.backend.model.Product;
+import com.ekhonni.backend.specification.ProductSpecification;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
+
+public class ProductSpecificationBuilder {
+    public static Specification<Product> build(ProductFilter filter, List<Long> categoryIds) {
+        Specification<Product> spec = Specification.where(null);
+        if (filter.getMinPrice() != null) {
+            spec = spec.and(ProductSpecification.hasMinimumPrice(filter.getMinPrice()));
+        }
+        if (filter.getMaxPrice() != null) {
+            spec = spec.and(ProductSpecification.hasMaximumPrice(filter.getMaxPrice()));
+        }
+        if (categoryIds != null && filter.getCategoryName() != null) {
+            spec = spec.and(ProductSpecification.belongsToCategories(categoryIds));
+        }
+        if (filter.getProductCondition() != null) {
+            spec = spec.and(ProductSpecification.hasCondition(ProductCondition.valueOf(filter.getProductCondition())));
+        }
+        if (filter.getSearchTerm() != null) {
+            spec = spec.and(ProductSpecification.hasTerm(filter.getSearchTerm()));
+        }
+        if (filter.getSortBy() != null) {
+            spec = spec.and(ProductSpecification.applySorting(filter.getSortBy()));
+        }
+
+        return spec;
+    }
+}
