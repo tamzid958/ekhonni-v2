9439786cd5618aa38dae7692b7729b38ca608e2e
Asif032 <asif01981872223@gmail.com>
2025-01-08 15:42:49 +0600

    replace cors-config with web-config

---
+++ backend/src/main/java/com/ekhonni/backend/config/WebConfig.java
@@ -0,0 +1,46 @@
+package com.ekhonni.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Author: Asif Iqbal
+ * Date: 1/7/25
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://127.0.0.1:3000",
+                        "http://host.docker.internal:3000"
+                )
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "PATCH",
+                        "OPTIONS"
+                )
+                .allowedHeaders(
+                        "Authorization",
+                        "Content-Type",
+                        "Accept",
+                        "Origin",
+                        "Access-Control-Request-Method",
+                        "Access-Control-Request-Headers"
+                )
+                .exposedHeaders(
+                        "Access-Control-Allow-Origin",
+                        "Access-Control-Allow-Credentials"
+                )
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
