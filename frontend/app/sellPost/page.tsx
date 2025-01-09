5839256aa3ad6f71d1492c044b09bb06157dca07
samiul-mugdha <mugdhasamiul@gmail.com>
2025-01-09 12:18:40 +0600

    ADD: demo form

--- frontend/app/sellPost/page.tsx
+++ frontend/app/sellPost/page.tsx
@@ -3,9 +3,18 @@
 import {Button} from '@/components/ui/button';
 import Link from "next/link";
 import React, {useState} from 'react';
+import {SubmitHandler, useForm} from "react-hook-form";
+
+interface IFormInput {
+    firstName: string
+    lastName: string
+    age: number
+}
 
 export default function sellPost() {
     const [formstate, setFormstate] = useState(0);
+    const {register, handleSubmit} = useForm<IFormInput>()
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
     return (
         <div>
             <Link href={'/sellPost/form1'}>
@@ -13,6 +22,12 @@ export default function sellPost() {
                     Sell Post
                 </Button>
             </Link>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register("firstName", {required: true, maxLength: 20})} />
+                <input {...register("lastName", {pattern: /^[A-Za-z]+$/i})} />
+                <input type="number" {...register("age", {min: 18, max: 99})} />
+                <input type="submit"/>
+            </form>
         </div>
     )
 }
