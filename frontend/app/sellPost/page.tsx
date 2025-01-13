2597a94d06d395407012edc732249ba74070652f
samiul-mugdha <mugdhasamiul@gmail.com>
2025-01-13 17:27:04 +0600

    ADD: sample form

--- frontend/app/sellPost/page.tsx
+++ frontend/app/sellPost/page.tsx
@@ -1,33 +1,165 @@
-"use client"
+'use client';
 
-import {Button} from '@/components/ui/button';
-import Link from "next/link";
-import React, {useState} from 'react';
-import {SubmitHandler, useForm} from "react-hook-form";
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
-interface IFormInput {
-    firstName: string
-    lastName: string
-    age: number
-}
+const formSchema = z
+  .object({
+    emailAddress: z.string().email(),
+    password: z.string().min(3),
+    passwordConfirm: z.string(),
+    accountType: z.enum(['personal', 'company']),
+    companyName: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirm;
+    },
+    {
+      message: 'Passwords do not match',
+      path: ['passwordConfirm'],
+    },
+  )
+  .refine(
+    (data) => {
+      if (data.accountType === 'company') {
+        return !!data.companyName;
+      }
+      return true;
+    },
+    {
+      message: 'Company name is required',
+      path: ['companyName'],
+    },
+  );
 
 export default function sellPost() {
-    const [formstate, setFormstate] = useState(0);
-    const {register, handleSubmit} = useForm<IFormInput>()
-    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
-    return (
-        <div>
-            <Link href={'/sellPost/form1'}>
-                <Button>
-                    Sell Post
-                </Button>
-            </Link>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("firstName", {required: true, maxLength: 20})} />
-                <input {...register("lastName", {pattern: /^[A-Za-z]+$/i})} />
-                <input type="number" {...register("age", {min: 18, max: 99})} />
-                <input type="submit"/>
-            </form>
-        </div>
-    )
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      emailAddress: '',
+      password: '',
+      passwordConfirm: '',
+      companyName: '',
+    },
+  });
+
+  const accountType = form.watch('accountType');
+
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log({ values });
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="max-w-md w-full flex flex-col gap-4"
+        >
+          <FormField
+            control={form.control}
+            name="emailAddress"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Email address</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Email address"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            control={form.control}
+            name="accountType"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Account type</FormLabel>
+                  <Select onValueChange={field.onChange}>
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select an account type" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="personal">Personal</SelectItem>
+                      <SelectItem value="company">Company</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          {accountType === 'company' && (
+            <FormField
+              control={form.control}
+              name="companyName"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Company name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Company name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          )}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Password confirm</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Password confirm"
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <Button type="submit" className="w-full">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </main>
+  );
 }
