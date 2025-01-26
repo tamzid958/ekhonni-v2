fc22fac2a2527f2d6e1ea2ae7b3dfde66a0358b7
ShahriarAlvi <alam.shahriaralvi@gmail.com>
2025-01-26 16:25:23 +0600

    UPDATE: admin product fetch/render separated

--- frontend/app/admin/categories/page.tsx
+++ frontend/app/admin/categories/page.tsx
@@ -1,5 +1,3 @@
-// 'use client';
-
 import React from 'react';
 import { fetchCategories } from './fetchCategories';
 import CategoryRender from './CategoryRender';
---
+++ frontend/app/admin/products/ProductRender.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import React, { useState } from 'react';
+import { HorizontalAdminCard } from '../components/HorizontalAdminCard';
+import { Separator } from '@/components/ui/separator';
+import { Button } from '@/components/ui/button';
+import { Package } from 'lucide-react';
+import { Data } from './fetchProducts';
+
+interface Props {
+  products: Data[];
+}
+
+export default function ProductRenderer({ products }: Props) {
+  const [filter, setFilter] = useState<string>('ALL');
+
+  const filteredProducts =
+    filter === 'ALL' ? products : products.filter((product) => product.status === filter);
+
+  return (
+    <div className="space-y-6 container mx-12 px-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-5xl font-bold my-8">Products</h1>
+
+        {/* Button Group */}
+        <div className="flex items-center justify-end space-x-4">
+          {['ALL', 'APPROVED', 'PENDING_APPROVAL', 'DECLINED', 'ARCHIVED'].map((status) => (
+            <Button
+              key={status}
+              onClick={() => setFilter(status)}
+              className={`px-4 py-2 rounded-lg ${
+                filter === status ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700 hover:bg-red-500 hover:text-white'
+              }`}
+            >
+              {status.replace('_', ' ')}
+            </Button>
+          ))}
+        </div>
+      </div>
+
+      <Separator />
+
+      {/* Products */}
+      <div className="space-y-6 mx-16">
+        {filteredProducts.length === 0 ? (
+          <div className="flex flex-col items-center justify-center text-gray-600">
+            <Package className="w-24 h-24 mb-4 text-gray-400" />
+            <p className="text-center font-semibold text-3xl">No products found</p>
+          </div>
+        ) : (
+          filteredProducts.map((product) => (
+            <div key={product.id} className="bg-white flex items-center justify-between pt-8">
+              <HorizontalAdminCard
+                id={parseInt(product.id)}
+                title={product.name}
+                img={product.images[0]?.imagePath || ''}
+                price={product.price}
+                shipping={0}
+                timeLeft="1d 5h"
+                condition={product.condition}
+                bidsAmount={0}
+                sellerProfile={product.seller.name}
+                status={product.status}
+              />
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
