f9e94477e566cd6cc88a336b02327754ad7776ff
Asif032 <asif01981872223@gmail.com>
2025-01-30 10:25:27 +0600

    edit api endpoint names in bid related to a product

--- backend/src/main/java/com/ekhonni/backend/controller/BidController.java
+++ backend/src/main/java/com/ekhonni/backend/controller/BidController.java
@@ -54,19 +54,19 @@ public class BidController {
         return ResponseUtil.createResponse(HTTPStatus.OK, bidService.getHighestBidAmount(productId));
     }
 
-    @GetMapping("/buyer/{product_id}")
+    @GetMapping("/buyer/product/{product_id}")
     public ResponseEntity<ApiResponse<Page<BuyerBidProjection>>> getAllForProductBuyer(
             @PathVariable("product_id") Long productId, Pageable pageable) {
         return ResponseUtil.createResponse(HTTPStatus.OK,
                 bidService.getAllForProduct(productId, BuyerBidProjection.class, pageable));
     }
 
-    @GetMapping("/{product_id}/count")
+    @GetMapping("/product/{product_id}/count")
     public ResponseEntity<ApiResponse<Long>> getCountForProduct(@PathVariable("product_id") Long productId) {
         return ResponseUtil.createResponse(HTTPStatus.OK, bidService.getCountForProduct(productId));
     }
 
-    @GetMapping("/seller/{product_id}")
+    @GetMapping("/seller/product/{product_id}")
     @PreAuthorize("@productService.getSellerId(#productId) == authentication.principal.id")
     public ResponseEntity<ApiResponse<Page<SellerBidProjection>>> getAllForProductSeller(
             @PathVariable("product_id") Long productId, Pageable pageable) {
@@ -108,8 +108,7 @@ public class BidController {
         return ResponseUtil.createResponse(HTTPStatus.OK, bidService.getAll(AdminBidProjection.class, pageable));
     }
 
-    @GetMapping("/admin/{product_id}")
-//    @PreAuthorize("hasAuthority('ADMIN', 'SUPER_ADMIN')")
+    @GetMapping("/admin/product/{product_id}")
     public ResponseEntity<ApiResponse<Page<AdminBidProjection>>> getAllForProduct(
             @PathVariable("product_id") Long productId, Pageable pageable) {
         return ResponseUtil.createResponse(HTTPStatus.OK,
@@ -118,14 +117,14 @@ public class BidController {
 
 
 
-    @GetMapping("/{product_id}/audit")
+    @GetMapping("/product/{product_id}/audit")
     public ResponseEntity<ApiResponse<Page<AdminBidProjection>>> getAuditForProduct(
             @PathVariable("product_id") Long productId, Pageable pageable) {
         return ResponseUtil.createResponse(HTTPStatus.OK,
                 bidService.getAuditForProduct(productId, AdminBidProjection.class, pageable));
     }
 
-    @GetMapping("/{product_id}/audit-count")
+    @GetMapping("/product/{product_id}/audit-count")
     public ResponseEntity<ApiResponse<Long>> getAuditCountForProduct(@PathVariable("product_id") Long productId) {
         return ResponseUtil.createResponse(HTTPStatus.OK, bidService.getAuditCountForProduct(productId));
     }
