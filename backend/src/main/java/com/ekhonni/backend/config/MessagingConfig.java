4d0c924a87a538c9a28d0e21cd98408587e288d4
Rifat Shariar Sakil <shariarsakil101@gmail.com>
2025-02-10 12:16:33 +0600

    chat config

---
+++ backend/src/main/java/com/ekhonni/backend/config/MessagingConfig.java
@@ -0,0 +1,23 @@
+///**
+// * Author: Rifat Shariar Sakil
+// * Time: 12:06 PM
+// * Date: 2/10/25
+// * Project Name: ekhonni-v2
+// */
+//
+//package com.ekhonni.backend.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.MessageHandler;
+//import org.springframework.messaging.simp.stomp.StompSubProtocolHandler;
+//
+//@Configuration
+//public class MessagingConfig {
+//
+//    @Bean
+//    public MessageHandler myMessageHandler() {
+//        return new StompSubProtocolHandler();
+//    }
+//}
+//
