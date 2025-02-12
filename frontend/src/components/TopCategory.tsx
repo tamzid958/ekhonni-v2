6a91786366a26e0765c6e0b4cfa878eeafc47e91
samiul-mugdha <mugdhasamiul@gmail.com>
2025-02-12 15:49:37 +0600

    UPDATE: NAVBAR and SEARCHBAR

--- frontend/src/components/TopCategory.tsx
+++ frontend/src/components/TopCategory.tsx
@@ -1,141 +1,141 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import {cn} from "@/lib/utils"
+import * as React from 'react';
+import { cn } from '@/lib/utils';
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import Link from "next/link";
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import Link from 'next/link';
 
 export function TopCAtegory() {
-    return (
-        <div className="bg-brand-mid h-[85px] flex justify-center">
-            <NavigationMenu>
-                <NavigationMenuList className="space-x-6">
-                    <NavigationMenuItem>
-                        <NavigationMenuTrigger>ALL CATEGORIES</NavigationMenuTrigger>
-                        <NavigationMenuContent>
-                            <ul className="text-b grid gap-2 p-4 md:w-[200px] lg:w-[300px]">
-                                <ListItem href="/docs" title="Electronics">
-                                    Wide range of tech products and accessories.
-                                </ListItem>
-                                <ListItem href="/docs/installation" title="Fashion">
-                                    Latest trends in clothing and accessories.
-                                </ListItem>
-                                <ListItem href="/docs" title="Sporting Goods">
-                                    Quality gear for all your sports needs.
-                                </ListItem>
-                                <ListItem href="/docs" title="Business">
-                                    Tools for growing and managing your business.
-                                </ListItem>
-                                <ListItem href="/docs" title="Collection and Art">
-                                    Curated art and collectible items for you.
-                                </ListItem>
-                                <ListItem href="/docs" title="Books and Movies">
-                                    Must-read books and top-rated movies.
-                                </ListItem>
+  return (
+    <div className="bg-brand-mid h-12 flex justify-center">
+      <NavigationMenu>
+        <NavigationMenuList className="space-x-6">
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>ALL CATEGORIES</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="text-b grid gap-1 p-4 md:w-[200px] lg:w-[300px]">
+                <ListItem href="/docs" title="Electronics">
+                  Wide range of tech products and accessories.
+                </ListItem>
+                <ListItem href="/docs/installation" title="Fashion">
+                  Latest trends in clothing and accessories.
+                </ListItem>
+                <ListItem href="/docs" title="Sporting Goods">
+                  Quality gear for all your sports needs.
+                </ListItem>
+                <ListItem href="/docs" title="Business">
+                  Tools for growing and managing your business.
+                </ListItem>
+                <ListItem href="/docs" title="Collection and Art">
+                  Curated art and collectible items for you.
+                </ListItem>
+                <ListItem href="/docs" title="Books and Movies">
+                  Must-read books and top-rated movies.
+                </ListItem>
 
-                            </ul>
-                        </NavigationMenuContent>
-                    </NavigationMenuItem>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
 
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                SNEAKERS
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                SNEAKERS
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
 
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                CLOTHES
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                CLOTHES
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
 
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                LUXURY
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                LUXURY
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
 
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                PERFUME
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                PERFUME
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
 
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                SPORTS
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                SPORTS
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
 
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                HEALTH
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                HEALTH
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
 
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                BEAUTY
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                BEAUTY
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
 
-                    <NavigationMenuItem>
-                        <Link href="/docs" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                GARDEN
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-        </div>
-    )
+          <NavigationMenuItem>
+            <Link href="/docs" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                GARDEN
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
 }
 
 const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
->(({className, title, children, ...props}, ref) => {
-    return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-brand-mid hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    )
-})
-ListItem.displayName = "ListItem"
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-brand-mid hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
+});
+ListItem.displayName = 'ListItem';
