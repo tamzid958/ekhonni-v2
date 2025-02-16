dbfe572e16f38380967b93a63b81123e2409492d
Asif032 <asif01981872223@gmail.com>
2025-02-16 17:53:00 +0600

    add service methods to find users by product id and status

--- backend/src/main/java/com/ekhonni/backend/repository/BidRepository.java
+++ backend/src/main/java/com/ekhonni/backend/repository/BidRepository.java
@@ -2,11 +2,13 @@ package com.ekhonni.backend.repository;
 
 import com.ekhonni.backend.enums.BidStatus;
 import com.ekhonni.backend.model.Bid;
+import com.ekhonni.backend.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -39,5 +41,9 @@ public interface BidRepository extends BaseRepository<Bid, Long> {
     <P> Page<P> findByProductId(Long productId, Class<P> projection, Pageable pageable);
 
     long countByProductId(Long productId);
+
+    User findByProductIdAndStatusAndDeletedAtIsNull(Long productId, BidStatus status);
+
+    User findByProductIdAndStatusInAndDeletedAtIsNull(Long productId, List<BidStatus> statuses);
 
 }
