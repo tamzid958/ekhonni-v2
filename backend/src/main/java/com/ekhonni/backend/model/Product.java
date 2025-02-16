6fe52bf341139852d8fe8bf3a427359182e9d397
Rifat Shariar Sakil <shariarsakil101@gmail.com>
2025-02-16 15:13:53 +0600

    insert buyer into product

--- backend/src/main/java/com/ekhonni/backend/model/Product.java
+++ backend/src/main/java/com/ekhonni/backend/model/Product.java
@@ -71,8 +71,8 @@ public class Product extends BaseEntity<Long> {
     @JoinColumn(name = "seller_id", nullable = false)
     private User seller;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "buyer_id", nullable = true)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id")
     private User buyer;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
@@ -83,13 +83,6 @@ public class Product extends BaseEntity<Long> {
     public ProductSellerAndBuyerDTO getSellerDTO() {
         return new ProductSellerAndBuyerDTO(this.getSeller().getId(), this.getSeller().getName());
     }
-
-    public ProductSellerAndBuyerDTO getBuyerDTO() {
-        return (this.getBuyer() != null)
-                ? new ProductSellerAndBuyerDTO(this.getBuyer().getId(), this.getBuyer().getName())
-                : null;
-    }
-
 
     public ProductCategoryDTO getCategoryDTO() {
         return new ProductCategoryDTO(this.getCategory().getId(), this.getCategory().getName());
