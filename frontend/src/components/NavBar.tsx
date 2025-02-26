cccf82e2478f1b94d10ba4b4a757c119911eccde
Md Sadman Hafiz <hafiz.sust333@gmail.com>
2025-02-26 12:15:50 +0600

    Update NavBar.tsx

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -58,7 +58,7 @@ export function NavBar({ placeholder }: Props) {
     const lastFetchTime = new Date(new Date().setDate(new Date().getDate() - 2)).toISOString().split('.')[0];
 
     async function fetchNotifications(lastFetchTime: string) {
-      console.log('Fetching notifications for users:', userId);
+      console.log('Fetching notifications for user:', userId);
       const result = await NotificationGetter(userId, userToken, lastFetchTime);
 
 
